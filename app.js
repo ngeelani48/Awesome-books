bebbe531ef54287489be7de9ad5ee0ad3b55b057
calebchris000 <calebchris000@gmail.com>
2023-02-14 18:22:05 +0100

    Prevent adding empty string

--- app.js
+++ app.js
@@ -1,61 +1,66 @@
-const bookCollection = document.querySelector('.bookshelf');
-const addButton = document.querySelector('.submit');
-const titleInput = document.getElementById('title');
-const authorInput = document.getElementById('author');
+const bookCollection = document.querySelector(".bookshelf");
+const addButton = document.querySelector(".submit");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
 
 const books = [];
 
-addButton.addEventListener('click', () => {
+addButton.addEventListener("click", () => {
   const titletext = titleInput.value;
   const authorText = authorInput.value;
 
+  if(titletext === '' || authorText == '') {
+    return
+  }
+
   books.push({ title: titletext, author: authorText });
-  let items = '';
+  let items = "";
 
   books.forEach((item, index) => {
-    items
-    += `<div class="coll${index}">
-    <p>${item.title}</p>
-    <p>${item.author}</p>
-    <button onclick="removeItem(${index})">remove</button>
+    items += `<div class="coll${index} collection">
+    <div class='bookinfo'>
+    <p class='title'>"${item.title}" by</p>
+    <p class='author'>${item.author}</p>
+    </div>
+    <button class='remove' onclick="removeItem(${index})">remove</button>
     </div>`;
   });
 
   bookCollection.innerHTML = items;
   /* eslint-disable no-use-before-define */
   saveData();
-  titleInput.value = '';
-  authorInput.value = '';
+  titleInput.value = "";
+  authorInput.value = "";
 });
 
 /* eslint-disable no-use-before-define */
 function saveData() {
   const stringify = JSON.stringify(books);
-  localStorage.setItem('data', stringify);
+  localStorage.setItem("data", stringify);
 }
 
 /* eslint-disable no-unused-vars */
 function removeItem(i) {
   books.splice(i, 1);
 
-  let items = '';
+  let items = "";
   books.forEach((item, index) => {
-    items
- += `<div class="coll${index}">
- <p>${item.title}</p>
- <p>${item.author}</p>
- <button onclick="removeItem(${index})">remove</button>
+    items += `<div class="coll${index} collection">
+ <div class='bookinfo'>
+ <p class='title'>${item.title} by</p>
+ <p class='author'>${item.author}</p>
+ </div>
+ <button class='remove' onclick="removeItem(${index})">remove</button>
  </div>`;
   });
 
-  bookCollection.innerHTML = '';
+  bookCollection.innerHTML = "";
   bookCollection.innerHTML = items;
 
-  // books =  books.filter(x=> books.indexOf(x) !== i);
   let stringify = JSON.stringify(books);
-  if (stringify === '[]') {
-    stringify = '';
+  if (stringify === "[]") {
+    stringify = "";
   }
 
-  localStorage.setItem('data', stringify);
+  localStorage.setItem("data", stringify);
 }
