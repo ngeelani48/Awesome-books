3e68817eb8e5fa4b9f846d5ac7e0a7ef02320062
calebchris000 <calebchris000@gmail.com>
2023-02-14 10:58:46 +0100

    Fix local storage not updating

--- app.js
+++ app.js
@@ -1,98 +1,61 @@
-// Button for the form section
 
-// /* eslint-disable */
 
-const addButton = document.querySelector(".submit");
+/* eslint disable */
+let bookCollection = document.querySelector('.bookshelf');
+let addButton = document.querySelector('.submit')
+let titleInput = document.getElementById('title')
+let authorInput = document.getElementById('author')
 
-// Div element for the bookshelf
-const bookshelf = document.querySelector(".bookshelf");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-
-// The collection that holds the author and the title
 let books = [];
 
-// Function template that creates the 'p' elements
-function titleCreation(element, text, parent) {
-  const item = document.createElement(element);
-  item.classList.add("text");
-  item.innerHTML = text;
-  parent.appendChild(item);
+
+addButton.addEventListener('click', function(){
+  let titletext = titleInput.value;
+  let authorText = authorInput.value
+
+  books.push({title: titletext, author: authorText})  
+  let items = '';
+
+  books.forEach((item, index) => {
+     items+=  
+    `<div class="coll${index}">
+    <p>${item.title}</p>
+    <p>${item.author}</p>
+    <button onclick="removeItem(${index})">remove</button>
+    </div>`;
+  })
+
+  bookCollection.innerHTML = items
+  saveData()
+  titleInput.value = ''
+  authorInput.value = ''
+})
+
+function saveData() {
+  let stringify = JSON.stringify(books);
+  localStorage.setItem('data', stringify)
 }
 
-function authorCreation(element, text, parent) {
-  const item = document.createElement(element);
-  item.classList.add("texty");
-  item.innerHTML = text;
-  parent.appendChild(item);
+function removeItem(i) {
+
+books.splice(i, 1)
+
+let items = ''
+books.forEach((item, index) => {
+ items+=  
+ `<div class="coll${index}">
+ <p>${item.title}</p>
+ <p>${item.author}</p>
+ <button onclick="removeItem(${index})">remove</button>
+ </div>`;
+})
+bookCollection.innerHTML = ''
+bookCollection.innerHTML = items
+
+  // books =  books.filter(x=> books.indexOf(x) !== i);
+ let stringify = JSON.stringify(books);
+ stringify == '[]'? stringify = '': 0;
+ localStorage.setItem('data', stringify)
+
 }
 
-// Event listener that appends the button to the html element
-/* eslint-disable */
-addButton.addEventListener("click", () => {
-  if (title.value !== "" || author.value !== "") {
-    const wrapper = document.createElement("div");
-    wrapper.classList.add("wrapper");
-    bookshelf.appendChild(wrapper);
-    titleCreation("p", title.value, wrapper);
-    authorCreation("p", author.value, wrapper);
-    createRemove("remove", wrapper);
-    toLocalStorage();
-    title.value = "";
-    author.value = "";
-  }
-});
-
-// Function that stores the data to the local storage
-function toLocalStorage() {
-  books.push({ Title: title.value, Author: author.value });
-  const stringified = JSON.stringify(books);
-  localStorage.setItem("data", stringified);
-}
-
-// Function that removes the data when the user presses the remove button
-function createRemove(text, parent) {
-  const removeButton = document.createElement("button");
-  removeButton.textContent = text;
-
-  removeButton.onclick = function () {
-    parent.removeChild(removeButton);
-    bookshelf.removeChild(parent);
-    updated();
-  };
-
-  parent.appendChild(removeButton);
-}
-
-function updated() {
-  books = []
-  let bookshelf = document.querySelector(".bookshelf").querySelectorAll('.wrapper');
-  let children = bookshelf.querySelectorAll(".text");
-  let children2 = bookshelf.querySelectorAll(".texty");
-  
-  for (let i = 0; i <bookshelf.length; i++) {
-
-    books.push({
-      'Title': children[i].textContent,
-      'Author': children2[i].textContent,
-    });
-  }
-  let item = JSON.stringify(books);
-  localStorage.setItem("data", item);
-}
-
-// Function to get the data from LocalStorage and
-// Append them back to the html
-function recover() {
-  const data = JSON.parse(localStorage.getItem("data"));
-  for (let i = 0; i < data.length; i++) {
-    titleCreation("p", data[i].Title, bookshelf);
-    titleCreation("p", data[i].Author, bookshelf);
-    const removeButton = document.createElement("button");
-    removeButton.textContent = "remove";
-    bookshelf.appendChild(removeButton);
-  }
-}
-
-// On browser interruption, the data is saved.
-window.onload = recover;
