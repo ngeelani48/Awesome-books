b8c5625acf05a30a72acde5830d4aa3f5f78efb3
calebchris000 <calebchris000@gmail.com>
2023-02-14 11:23:56 +0100

    Rewrite the app.js code to fix multiple bugs

--- app.js
+++ app.js
@@ -1,98 +1,61 @@
-// Button for the form section
+const bookCollection = document.querySelector('.bookshelf');
+const addButton = document.querySelector('.submit');
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
 
-// /* eslint-disable */
+const books = [];
 
-const addButton = document.querySelector(".submit");
+addButton.addEventListener('click', () => {
+  const titletext = titleInput.value;
+  const authorText = authorInput.value;
 
-// Div element for the bookshelf
-const bookshelf = document.querySelector(".bookshelf");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
+  books.push({ title: titletext, author: authorText });
+  let items = '';
 
-// The collection that holds the author and the title
-let books = [];
+  books.forEach((item, index) => {
+    items
+    += `<div class="coll${index}">
+    <p>${item.title}</p>
+    <p>${item.author}</p>
+    <button onclick="removeItem(${index})">remove</button>
+    </div>`;
+  });
 
-// Function template that creates the 'p' elements
-function titleCreation(element, text, parent) {
-  const item = document.createElement(element);
-  item.classList.add("text");
-  item.innerHTML = text;
-  parent.appendChild(item);
-}
-
-function authorCreation(element, text, parent) {
-  const item = document.createElement(element);
-  item.classList.add("texty");
-  item.innerHTML = text;
-  parent.appendChild(item);
-}
-
-// Event listener that appends the button to the html element
-/* eslint-disable */
-addButton.addEventListener("click", () => {
-  if (title.value !== "" || author.value !== "") {
-    const wrapper = document.createElement("div");
-    wrapper.classList.add("wrapper");
-    bookshelf.appendChild(wrapper);
-    titleCreation("p", title.value, wrapper);
-    authorCreation("p", author.value, wrapper);
-    createRemove("remove", wrapper);
-    toLocalStorage();
-    title.value = "";
-    author.value = "";
-  }
+  bookCollection.innerHTML = items;
+  /* eslint-disable no-use-before-define */
+  saveData();
+  titleInput.value = '';
+  authorInput.value = '';
 });
 
-// Function that stores the data to the local storage
-function toLocalStorage() {
-  books.push({ Title: title.value, Author: author.value });
-  const stringified = JSON.stringify(books);
-  localStorage.setItem("data", stringified);
+/* eslint-disable no-use-before-define */
+function saveData() {
+  const stringify = JSON.stringify(books);
+  localStorage.setItem('data', stringify);
 }
 
-// Function that removes the data when the user presses the remove button
-function createRemove(text, parent) {
-  const removeButton = document.createElement("button");
-  removeButton.textContent = text;
+/* eslint-disable no-unused-vars */
+function removeItem(i) {
+  books.splice(i, 1);
 
-  removeButton.onclick = function () {
-    parent.removeChild(removeButton);
-    bookshelf.removeChild(parent);
-    updated();
-  };
-
-  parent.appendChild(removeButton);
-}
-
-function updated() {
-  books = []
-  let bookshelf = document.querySelector(".bookshelf").querySelectorAll('.wrapper');
-  let children = bookshelf.querySelectorAll(".text");
-  let children2 = bookshelf.querySelectorAll(".texty");
+  let items = '';
+  books.forEach((item, index) => {
+    items
+ += `<div class="coll${index}">
+ <p>${item.title}</p>
+ <p>${item.author}</p>
+ <button onclick="removeItem(${index})">remove</button>
+ </div>`;
+  });
   
-  for (let i = 0; i <bookshelf.length; i++) {
+  bookCollection.innerHTML = '';
+  bookCollection.innerHTML = items;
 
-    books.push({
-      'Title': children[i].textContent,
-      'Author': children2[i].textContent,
-    });
+  // books =  books.filter(x=> books.indexOf(x) !== i);
+  let stringify = JSON.stringify(books);
+  if (stringify === '[]') {
+    stringify = '';
   }
-  let item = JSON.stringify(books);
-  localStorage.setItem("data", item);
-}
 
-// Function to get the data from LocalStorage and
-// Append them back to the html
-function recover() {
-  const data = JSON.parse(localStorage.getItem("data"));
-  for (let i = 0; i < data.length; i++) {
-    titleCreation("p", data[i].Title, bookshelf);
-    titleCreation("p", data[i].Author, bookshelf);
-    const removeButton = document.createElement("button");
-    removeButton.textContent = "remove";
-    bookshelf.appendChild(removeButton);
-  }
+  localStorage.setItem('data', stringify);
 }
-
-// On browser interruption, the data is saved.
-window.onload = recover;
